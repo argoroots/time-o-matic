6ce8d3c42e9d75a2e5316cc0b6358fe6ecd6655a
Argo Roots <argo.roots@adm.ee>
2021-09-22 10:00:18 +0300

    Add copy to clipboard

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,26 @@
         disabled
         placeholder="..."
       >
+      <div
+        class="text-center my-2 cursor-pointer text-gray-500 hover:text-blue-600"
+        title="Copy to Clipboard"
+        @click="copyResult()"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 inline"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+          />
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -67,5 +87,9 @@ const newTime = computed(() => {
   const hours = Math.round(multiplierFloat * minutes / 0.6) / 100
   return hours ? `${hours}h` : ''
 })
+
+const copyResult = () => {
+  navigator.clipboard.writeText(newTime.value)
+}
 
 </script>
